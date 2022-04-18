9c415d91fed5e29a55a57bfd0c6fda22be209d55
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-04-18 11:43:57 +0100

    First Commit

--- src/DBConnection/DataBaseConnection.java
+++ src/DBConnection/DataBaseConnection.java
@@ -4,10 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-
-import static com.sun.org.apache.xml.internal.security.keys.keyresolver.KeyResolver.iterator;
 
 public class DataBaseConnection {
     private static final String url="jdbc:postgresql://localhost:5432/libdb";
